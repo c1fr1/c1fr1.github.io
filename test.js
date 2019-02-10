23f55055aa4039f86897caf9528f91163c5a8e8d
c1fr1 <c1fr1.stkcps@gmail.com>
2019-02-10 04:09:34 +0000

    added vao support, external shaders, now testing images

--- test.js
+++ test.js
@@ -1,30 +1,79 @@
 class VBO {
-  constructor(gl, vertices) {
+  //int id;
+  //int dim;
+  constructor(gl, vertices, dim) {
     this.id = gl.createBuffer();
+    this.dim = dim;
     gl.bindBuffer(gl.ARRAY_BUFFER, this.id);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
   }
+  bind(gl, loc) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.id);
+    gl.vertexAttribPointer(loc, this.dim, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(loc);
+  }
+}
+class IBO {
+  //int id;
+  constructor(gl, indices) {
+    this.id = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.id);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+  }
+  bind(gl) {
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.id);
+  }
+}
+class VAO {
+  //VBO[] vbos;
+  //IBO ibo;
+  //int indexCount;
+  constructor(vbos, ibo, indexCount) {
+    this.vbos = vbos;
+    this.ibo = ibo;
+    this.indexCount = indexCount;
+  }
+  static makeVertices(gl, vertices, dim, indices) {
+    return new VAO([new VBO(gl, vertices, dim)], new IBO(gl, indices), indices.length);
+  }
+  static makeSquare(gl, width, height) {
+    const vertices = [
+    -width/2,  height/2,
+     width/2,  height/2,
+    -width/2, -height/2,
+     width/2, -height/2
+    ]
+    const colors = [
+    1.0, 1.0, 1.0,
+    1.0, 0.0, 0.0,
+    0.0, 1.0, 0.0,
+    0.0, 0.0, 1.0
+    ]
+    const indices = [0, 1, 2, 1, 2, 3];
+    return new VAO([new VBO(gl, vertices, 2), new VBO(gl, colors, 3)], new IBO(gl, indices), indices.length);
+    /*console.log(vertices);
+    return new VAO([new VBO(gl, vertices, 2)], new IBO(gl, [0, 1, 2, 1, 2 ,3], 6));*/
+  }
+  prepareRender(gl, program) {
+    for (var i = 0; i < this.vbos.length && i < program.attribPositions.length; ++i) {
+      this.vbos[i].bind(gl, program.attribPositions[i]);
+    }
+    this.ibo.bind(gl);
+  }
+  draw(gl) {
+    gl.drawElements(gl.TRIANGLES, this.indexCount, gl.UNSIGNED_SHORT, 0);
+  }
+  fullRender(gl, program) {
+    this.prepareRender(gl, program);
+    this.draw(gl);
+  }
 }
 class ShaderProgram {
-  constructor(gl, vert, frag) {
-    const vertShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertShader, vert);
-    gl.compileShader(vertShader);
-    if (!gl.getShaderParameter(vertShader, gl.COMPILE_STATUS)) {
-      alert('An error occurred compiling the vertex shader: ' + gl.getShaderInfoLog(vertShader));
-      gl.deleteShader(vertShader);
-      this.id = -1;
-      return;
-    }
-    const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragShader, frag);
-    gl.compileShader(fragShader);
-    if (!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS)) {
-      alert('An error occurred compiling the fragment shader: ' + gl.getShaderInfoLog(fragShader));
-      gl.deleteShader(fragShader);
-      this.id = -1;
-      return;
-    }
+  //int id;
+  //int[] attribPositions;
+  constructor(gl, vert, frag, attribNames) {
+    const vertShader = ShaderProgram.loadShader(gl, gl.VERTEX_SHADER, vert);
+    const fragShader = ShaderProgram.loadShader(gl, gl.FRAGMENT_SHADER, frag);
 
     this.id = gl.createProgram();
     gl.attachShader(this.id, vertShader);
@@ -35,13 +84,61 @@ class ShaderProgram {
       alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(this.id));
       this.id = -1;
     }
+    this.attribPositions = [];
+    for (var i = 0; i < attribNames.length;++i) {
+      this.attribPositions.push(gl.getAttribLocation(this.id, attribNames[i]));
+    }
+  }
+  static makeProgram(gl, name, attribNames) {
+    return new ShaderProgram(gl, document.querySelector("#" + name + "-vert").textContent, document.querySelector("#" + name + "-frag").textContent, attribNames);
+  }
 
-    this.vertAttribLocation = gl.getAttribLocation(this.id, "vert");
+  static loadShader(gl, type, source) {
+    const id = gl.createShader(type);
+    gl.shaderSource(id, source);
+    gl.compileShader(id);
+    if (!gl.getShaderParameter(id, gl.COMPILE_STATUS)) {
+      alert("An error occurred compiling shader with type: " + type + "error: " + gl.getShaderInfoLog(vertShader));
+      gl.deleteShader(id);
+      this.id = -1;
+      return;
+    }
+    return id;
   }
   enable(gl) {
+    gl.useProgram(this.id);
   }
 }
-function main() {
+
+class Texture {
+  //int id;
+  //boolean loaded;
+  constructor(gl, url) {
+    const id = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, id);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 255, 255]));
+  
+    const image = new Image();
+    console.log("kek");
+    image.onload = function() {
+      gl.bindTexture(gl.TEXTURE_2D, id);
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+  
+      if ((image.width & (image.width - 1)) == 0 && (image.height & (image.height - 1)) == 0) {
+         gl.generateMipmap(gl.TEXTURE_2D);
+      } else {
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+      }
+      console.log("kek");
+    };
+    image.src = url;
+    this.id = id;
+  }
+}
+
+function glSetup() {
   const canvas = document.querySelector("#glCanvas");
   const gl = canvas.getContext("webgl");
   if (gl === null) {
@@ -49,34 +146,78 @@ function main() {
     return;
   }
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  return gl;
+}
+
+function main() {
+  const gl = glSetup();
   gl.clear(gl.COLOR_BUFFER_BIT);
-
-  const vsSource = `
-    attribute vec2 vert;
-
-    void main() {
-      gl_Position = vec4(vert, 0, 1);
-    }
-  `;
-  const fsSource = `
-    void main() {
-      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-    }
-  `;
-
-  var program = new ShaderProgram(gl, vsSource, fsSource);
-  var vertexBuffer = new VBO(gl, [-0.5, 0.5, 0.5, 0.5, -0.5, -0.5]);
-
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
   
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer.id);
-  gl.vertexAttribPointer(program.vertAttribLocation, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(program.vertAttribLocation);
-  
-  gl.useProgram(program.id);
+  var program = ShaderProgram.makeProgram(gl, "normal", ["vert", "clr"]);
+  const vao = VAO.makeSquare(gl, 1, 1);
 
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3);
+  gl.clear(gl.COLOR_BUFFER_BIT);
   
+  program.enable(gl);
+
+  vao.fullRender(gl, program);
+  
+  loadTexture(gl, "file:///C:/Users/Varas/Programming/website/favicon.png");
+  const tex = new Texture(gl, "file:///C:/Users/Varas/Programming/website/favicon.png");
 }
 main();
+
+
+
+
+
+
+function loadTexture(gl, url) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Because images have to be download over the internet
+  // they might take a moment until they are ready.
+  // Until then put a single pixel in the texture so we can
+  // use it immediately. When the image has finished downloading
+  // we'll update the texture with the contents of the image.
+  const level = 0;
+  const internalFormat = gl.RGBA;
+  const width = 1;
+  const height = 1;
+  const border = 0;
+  const srcFormat = gl.RGBA;
+  const srcType = gl.UNSIGNED_BYTE;
+  const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
+  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                width, height, border, srcFormat, srcType,
+                pixel);
+
+  const image = new Image();
+  image.onload = function() {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                  srcFormat, srcType, image);
+
+    // WebGL1 has different requirements for power of 2 images
+    // vs non power of 2 images so check if the image is a
+    // power of 2 in both dimensions.
+    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+       // Yes, it's a power of 2. Generate mips.
+       gl.generateMipmap(gl.TEXTURE_2D);
+    } else {
+       // No, it's not a power of 2. Turn off mips and set
+       // wrapping to clamp to edge
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    }
+  };
+  image.src = url;
+
+  return texture;
+}
+
+function isPowerOf2(value) {
+  return (value & (value - 1)) == 0;
+}
